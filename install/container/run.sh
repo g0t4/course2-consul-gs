29a221a49655e194c53888cff2a42173725b18e8
Wes Higbee <wes.mcclure@gmail.com>
2022-03-22 06:16:19 -0500

    run.sh to create install env

---
+++ install/container/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker container run --name install_env \
+  -it install_env
+
+# use download page for latest install instructions
+# https://www.consul.io/downloads
+# modify for container env
