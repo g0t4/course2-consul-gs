f8d0e502edc84170335085ef37088733af7924d2
Wes Higbee <wes.mcclure@gmail.com>
2022-04-03 05:32:02 -0500

    remove todo for now

--- services/orders/src/shipments.mjs
+++ services/orders/src/shipments.mjs
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const shipmentsClient = {
   getShipmentsForOrder(orderId) {
-    // TODO query HTTP API example - i.e. http://localhost:8500/
-    // TODO v1/catalog/service/shipments (addy vs service addy, port...)
     return axios.get("http://shipments:5000/shipments");
   },
 };
