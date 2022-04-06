93c9f1d3d5e6629113d2c415d65df4db88673a49
Wes Higbee <wes.mcclure@gmail.com>
2022-04-05 19:08:15 -0500

    add command for installing autocomplete and links to source to see how it works per shell type

--- install/vm/provision/install.consul.sh
+++ install/vm/provision/install.consul.sh
@@ -3,9 +3,13 @@
 sudo curl -fsSL https://apt.releases.hashicorp.com/gpg \
   --output /etc/apt/trusted.gpg.d/hashicorp.asc
 
-sudo apt-add-repository \
+sudo apt-add-repository -y \
   "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
 sudo apt-get update 
 
 sudo apt-get install -y consul
+
+# https://www.consul.io/commands#autocompletion
+consul -autocomplete-install # adds to bashrc 
+# source: https://github.com/posener/complete/blob/v1/cmd/install/install.go#L81
