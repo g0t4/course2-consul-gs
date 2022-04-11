470b8edf732c903c1fe025a56885ae7e1fb0fb83
Wes Higbee <wes.mcclure@gmail.com>
2022-04-10 21:27:31 -0500

    skeleton of discover demo env that will be built up through registration/discovery demos

---
+++ discover/provision/install.consul.sh
@@ -0,0 +1,11 @@
+# adapted from https://consul.io/downloads
+
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg \
+  --output /etc/apt/trusted.gpg.d/hashicorp.asc
+
+sudo apt-add-repository -y \
+  "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update 
+
+sudo apt-get install -y consul
