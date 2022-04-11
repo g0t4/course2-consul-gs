c606a057aabed1e84a8aae6f83817831338c60c5
Wes Higbee <wes.mcclure@gmail.com>
2022-04-11 06:50:47 -0500

    note about optionally creating docker contexts for each vagrant VM - also possible thanks to vagrant ssh-config > ...

--- discover/up.sh
+++ discover/up.sh
@@ -8,3 +8,8 @@ vagrant up --parallel
 # - also useful for ssh docker context (if needed)
 # - assumes config.d config files included in ssh config
 vagrant ssh-config > ~/.ssh/config.d/vm-testing.config
+
+# optional: create docker contexts
+# TODO: somewhere I have code to generate these from vagrant status command (look at swarm course)
+# docker context create --docker "host=ssh://s1" s1
+# - then: `docker -c s1 container ps` or `docker context use s1` 
