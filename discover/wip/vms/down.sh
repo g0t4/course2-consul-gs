ef74a0593d6c0b7760e89a4ff230ebcb81dfd09d
Wes Higbee <wes.mcclure@gmail.com>
2022-04-21 18:32:17 -0500

    remove wip samples not used in module

--- discover/wip/vms/down.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# manual confirm to nuke VMs
-vagrant destroy
-
-# remove ssh config to vms
-trash ~/.ssh/config.d/vm-testing.config
-
-echo "use 'vagrant box rm' to remove boxes when done with all demos"
