47ba819027cb22f5126b7f5b3693e97557344060
Wes Higbee <wes.mcclure@gmail.com>
2022-04-22 09:09:52 -0500

    set request timeout to shipments service

--- services/orders/src/shipments.mjs
+++ services/orders/src/shipments.mjs
@@ -3,7 +3,13 @@ import { config } from "./config.mjs";
 
 const shipmentsClient = {
   getShipmentsForOrder(orderId) {
-    return axios.get(`${config.SHIPMENTS_URL}/shipments`);
+    return axios.get(`${config.SHIPMENTS_URL}/shipments`, {
+      // request timeout
+      timeout: 5000, // ensure watch demos don't hang for 30-60sec or longer
+      // - timeout is generic but works (shows failure)
+      // - 5 seconds should be plenty for my docker demos
+      // on timeout - just make another request until meaningful message (ie unreachable after taking service offline)
+    });
   },
 };
 
