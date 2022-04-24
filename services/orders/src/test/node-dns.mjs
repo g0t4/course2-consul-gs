033031ca43baab7b0c51942dcc2d459ca4bf0794
Wes Higbee <wes.mcclure@gmail.com>
2022-04-24 17:00:18 -0500

    notes

--- services/orders/src/test/node-dns.mjs
+++ services/orders/src/test/node-dns.mjs
@@ -15,5 +15,6 @@ console.log(`resolveAny('${host}')`, await r.resolveAny(host));
 // https://nodejs.org/dist/latest-v18.x/docs/api/dns.html#dnspromiseslookuphostname-options
 
 console.log(`lookup('${host}')`, await lookup(host));
+// ok not going to use lookup as Resolver also works with /etc/hosts entries, docs make it sound like only lookup supports that but maybe I misread them.
 
 // NOTE: DNS queries are non-deterministic
