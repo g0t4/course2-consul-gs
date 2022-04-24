c153773ae31cd8ba74527f1b4c84685ed174220d
Wes Higbee <wes.mcclure@gmail.com>
2022-04-24 17:14:14 -0500

    more tinkering with SRV records

--- services/orders/src/test/node-dns.mjs
+++ services/orders/src/test/node-dns.mjs
@@ -10,11 +10,24 @@ if (dnsServer && dnsServer !== "") {
 console.log("getServers", r.getServers());
 
 console.log(`resolve('${host}')`, await r.resolve(host));
-console.log(`resolveAny('${host}')`, await r.resolveAny(host));
+// console.log(`resolveAny('${host}')`, await r.resolveAny(host));
 
 // https://nodejs.org/dist/latest-v18.x/docs/api/dns.html#dnspromiseslookuphostname-options
 
 console.log(`lookup('${host}')`, await lookup(host));
 // ok not going to use lookup as Resolver also works with /etc/hosts entries, docs make it sound like only lookup supports that but maybe I misread them.
+
+// ask for SRV directly, works for shipments.service.consul but not smtp.service.consul
+// const rr = await r.resolve(host, "SRV");
+// console.log('rr', rr);
+// process.exit();
+
+const SRV_records = await r.resolveSrv(host);
+console.log(`resolveSrv('${host}')`, SRV_records);
+if (SRV_records.length) {
+  const firstRecord = SRV_records[0];
+  const port = firstRecord.port;
+  const host = firstRecord.name;
+}
 
 // NOTE: DNS queries are non-deterministic
