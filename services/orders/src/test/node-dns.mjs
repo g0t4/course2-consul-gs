770b05cbe47d952bab1fb1d8004310df2c7e8a4f
Wes Higbee <wes.mcclure@gmail.com>
2022-04-24 17:19:18 -0500

    not using lookup for now

--- services/orders/src/test/node-dns.mjs
+++ services/orders/src/test/node-dns.mjs
@@ -14,7 +14,7 @@ console.log(`resolve('${host}')`, await r.resolve(host));
 
 // https://nodejs.org/dist/latest-v18.x/docs/api/dns.html#dnspromiseslookuphostname-options
 
-console.log(`lookup('${host}')`, await lookup(host));
+// console.log(`lookup('${host}')`, await lookup(host));
 // ok not going to use lookup as Resolver also works with /etc/hosts entries, docs make it sound like only lookup supports that but maybe I misread them.
 
 // ask for SRV directly, works for shipments.service.consul but not smtp.service.consul
