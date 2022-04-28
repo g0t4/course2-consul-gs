c48bc429bbfdff152c81dd62ebad98ed6457d616
Wes Higbee <wes.mcclure@gmail.com>
2022-04-28 13:09:29 -0500

    fix

--- kv/cleanup.sh
+++ kv/cleanup.sh
@@ -1,5 +1,5 @@
 # kill watches so we don't docker compose run new containers
-tmux kill-sessions -t kv 
+tmux kill-session -t kv 
 
 # cleanup all compose project resources
 docker compose down --remove-orphans \
