1da3dfd52651fe05618f15377de8379845224a4d
Wes Higbee <wes.mcclure@gmail.com>
2022-05-01 18:42:34 -0500

    pick apart starting demo - step 1 - convert dev mode agent to server mode

--- nodes/cleanup.sh
+++ nodes/cleanup.sh
@@ -1,6 +1,3 @@
-# kill watches so we don't docker compose run new containers
-tmux kill-sessions -t checks
-
 # cleanup all compose project resources
 docker compose down --remove-orphans \
   --timeout 0 \
