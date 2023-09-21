abd4f653fad6cbc63a25964c90509cc4b37551ae
Wes Higbee <wes.mcclure@gmail.com>
2023-09-21 12:26:49 -0500

    switch to consul's verified image with the version of consul when course was created: hashicorp/consul:v1.11.4 - see #1 for details

--- install/container/container.sh
+++ install/container/container.sh
@@ -1,4 +1,8 @@
 # run consul agent via official image
 docker container run --rm -it \
   -p 8500:8500 -p 8600:8600 -p 8600:8600/udp \
-  consul
+  hashicorp/consul:1.11.4
+
+## test if works:
+# curl localhost:8500/v1/agent/self
+# dig @127.0.0.1 -p 8600 consul.service.consul.
