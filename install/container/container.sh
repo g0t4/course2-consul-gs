c4c42d17c36efbf499f36a99021dd7058a5362c2
Wes Higbee <wes.mcclure@gmail.com>
2023-09-21 13:25:56 -0500

    note verified image:

--- install/container/container.sh
+++ install/container/container.sh
@@ -1,4 +1,5 @@
-# run consul agent via official image
+# run consul agent via HashiCorp's verified image (https://hub.docker.com/r/hashicorp/consul):
+#   NOTE: consul also has an official image on Docker Hub that is no longer receiving updates as of v1.16: https://hub.docker.com/_/consul
 docker container run --rm -it \
   -p 8500:8500 -p 8600:8600 -p 8600:8600/udp \
   hashicorp/consul:1.12.0
